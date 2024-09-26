bcb7fa42f36b15ca1a7d990ac1d3c5647c4786fb
oodegard <oodegard@gmail.com>
2024-09-26 08:27:15 +0200

    add plotly with Fractions. Read all files in   folder

--- read_akta_csv.R
+++ read_akta_csv.R
@@ -1,11 +1,13 @@
 library(reshape2)  # For melting the data into long format
 library(ggplot2)
 library(yaml)
+library(plotly)
+library(dplyr)
 
 
 #csvfile = path_to_csvfile
 
-aktacsvreader <- function(csvfile) {
+aktacsvreader <- function(csvfile, plot_name) {
   # Read the CSV file
   data <- read.csv(csvfile, stringsAsFactors = FALSE, skip = 1)
   
@@ -32,7 +34,7 @@ aktacsvreader <- function(csvfile) {
     measurement <- as.numeric(data[-1, col])
     
     # Create a data frame with 'volume' and the measurement column named as 'data_name'
-    df <- data.frame(volume = volume, measurement)
+    df <- data.frame(volume = volume, measurement = measurement, plot_name = plot_name)
     colnames(df)[2] <- data_name
     
     # Remove rows where the 'volume' is NA
@@ -47,19 +49,18 @@ aktacsvreader <- function(csvfile) {
 
 
 # set working directory
-working_dir = "C:/Users/oodeg/OneDrive - Universitetet i Oslo/Work/03_UiO/15_instrument_data/Akta"
+working_dir = c("C:/Users/oodeg/OneDrive - Universitetet i Oslo/Work/03_UiO/15_instrument_data/Akta",
+                "C:/Users/Øyvind/OneDrive - Universitetet i Oslo/Work/03_UiO/15_instrument_data/Akta")
+
+working_dir = working_dir[file.exists(working_dir)]
 setwd(working_dir)
 
 
-# Read files
+# Define files to use
 csvfiles = list.files(working_dir, pattern = ".csv$" )
-data_tables = lapply(csvfiles, aktacsvreader)
-names(data_tables) <- basename(csvfiles)
 
 # Make yaml file for defining plot naming
-
 yaml_file_path = "experiment_info.yaml"
-
 if(!file.exists(yaml_file_path)){
   fill_in_text = "Please_fill_in_name_to_be_shown_in_plot"
   yamlfile = lapply(csvfiles, function(x){
@@ -70,8 +71,6 @@ if(!file.exists(yaml_file_path)){
   })
   write_yaml(yamlfile, "experiment_info.yaml")
 }
-
-
 yamlfile = read_yaml(yaml_file_path)
 
 #TODO
@@ -85,11 +84,15 @@ yamlfile = read_yaml(yaml_file_path)
 # })
 
 
-# Merge table into one big data.frame
+# Read data and make data table
+i = 1
+data_tables = lapply(1:length(csvfiles), function(i){
+  aktacsvreader(csvfiles[[i]], yamlfile[[i]]$plot_name)
+})
+names(data_tables) <- basename(csvfiles)
 
+# Extract UV data
 df_uv = data.frame()
-
-#i =1
 for (i in 1:length(data_tables)) {
   dt = data_tables[[i]]
   # UV
@@ -97,11 +100,51 @@ for (i in 1:length(data_tables)) {
   df_uv <- rbind(df_uv, uv)
 }
 
-library(plotly)
+#Extract Fraction data
+df_Fractions = data.frame()
+for (i in 1:length(data_tables)) {
+  dt = data_tables[[i]]
+  # UV
+  Fractions = data.frame(volume = dt$Fraction$volume, Fraction = dt$Fraction$Fraction, name = yamlfile[[i]]$plot_name )
+  # The fractions is offset by the injection volume
+  # And each fraction starts from the value after that adjustment until the next value in the list
+  Fractions$volume = Fractions$volume -  data_tables[[i]]$Injection$volume
+  
+  df_Fractions <- rbind(df_Fractions, Fractions)
+}
 
-# Assuming df_uv is already created
-p <- ggplot(df_uv) +
-  geom_line(aes(x = volume, y = uv, col = name))
+# Summarize the mean and standard deviation volume for each fraction across all experiments
+df_Fractions_mean <- df_Fractions %>%
+  select(-name) %>%  # Remove the 'name' column
+  group_by(Fraction) %>%
+  summarise(
+    mean_volume = mean(volume, na.rm = TRUE),
+    sd_volume = sd(volume, na.rm = TRUE)
+  )
+
+fraction_size_mean = mean(c(Fractions$volume, NA) -c(NA, Fractions$volume), na.rm = TRUE)
+ 
+
+fractions_divisible_by_5 <- df_Fractions_mean[!is.na(df_Fractions_mean$Fraction) & df_Fractions_mean$Fraction %% 5 == 0,]
+
+fractions_to_plot = rbind(df_Fractions_mean[1,],fractions_divisible_by_5 )
+
+p <- ggplot() +
+  geom_vline(xintercept = Fractions$volume,
+             col = "grey",
+             linetype = "dotted"
+             ) +
+  geom_line(
+    data = df_uv,
+    aes(x = volume, y = uv, col = name)
+  )+
+  geom_text(aes(x = fractions_to_plot$mean_volume + (fraction_size_mean/2),
+                y = -5),
+                color ="red",
+                size = 4,
+                label = fractions_to_plot$Fraction ) +
+  theme_minimal() 
+
 
 # Convert to plotly interactive plot
 ggplotly(p)
